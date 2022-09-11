77a06b340b6e7655c083d55fd4ade2b7f04f7522
Avjol Sakaj <avjol.sakaj@cigen.me>
2022-09-11 10:53:31 +0200

    Add project files.

---
+++ AdvancedFeatues.Presantation.One/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace AdvancedFeatues.Presantation.One;
+
+internal class Program
+{
+    private static void Main (string[] args)
+    {
+        Console.WriteLine("We have best class ever!");
+
+        var child = new Child();
+        child.MethodOne();
+        child.Method();
+
+        _ = Console.ReadLine();
+    }
+}
+
+public class Base
+{
+    internal void Method ()
+    {
+        Console.WriteLine("This is comming from Base Class!");
+    }
+}
+
+public class Child : Base
+{
+    public void MethodOne ()
+    {
+        Console.WriteLine("This is comming from child");
+    }
+}
