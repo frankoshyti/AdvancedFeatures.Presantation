6bc2c725b4a25a3018d24c32e5654f89e7f0c6f1
Avjol Sakaj <avjol.sakaj@cigen.me>
2022-09-12 16:27:16 +0200

    First class demo

---
+++ AdvancedFeatues.Subclasses/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace AdvancedFeatues.Subclasses
+{
+    internal class Program
+    {
+        private static void Main (string[] args)
+        {
+            var oild = true;
+            var instType = "Trumpet";
+            var clean = false;
+
+            _ = new Trumpet(oild, instType, clean);
+
+            _ = Console.ReadLine();
+        }
+    }
+
+    public class Instument
+    {
+        private string type;
+        private bool clean;
+
+        public Instument (bool clean, string type)
+        {
+            this.type = type;
+            this.clean = clean;
+        }
+    }
+
+    public class Trumpet : Instument
+    {
+        private bool oiled;
+
+        public Trumpet () : base(false, "String Type")
+        {
+
+        }
+
+        public Trumpet (bool oiled, string type, bool clean) : base(clean, type)
+        {
+            this.oiled = oiled;
+        }
+    }
+
+    public class TrampetName : Trumpet
+    {
+        public TrampetName ()
+        {
+        }
+
+        public TrampetName (bool oiled, string type, bool clean) : base(oiled, type, clean)
+        {
+        }
+    }
+
+    public class TrampetName2 : Trumpet
+    {
+        public TrampetName2 (bool oiled, string type, bool clean) : base()
+        {
+
+        }
+    }
+}
---
+++ AdvancedFeatures.Polymorphism/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace AdvancedFeatures.Polymorphism;
+
+internal class Program
+{
+    private static void Main (string[] args)
+    {
+        var dog = new Dog();
+        dog.Age = 2;
+        dog.Name = "Bali";
+
+        //var dog = new Dog
+        //{
+        //    Age = 2,
+        //    Name = "Bali"
+        //};
+
+        dog.GetName();
+        //Operation.Call(dog);
+
+        var cat = new Cat
+        {
+            Age = 5,
+            Name = "Lusika"
+        };
+
+        cat.GetName();
+
+        //Operation.Call(cat);
+
+        _ = Console.ReadLine();
+    }
+}
+
+public class Animal
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public virtual void GetName ()
+    {
+        Console.WriteLine("This method can be overriden");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void GetName ()
+    {
+        Console.WriteLine($"Dog name is {Name}, and age is {Age}");
+
+        base.GetName();
+    }
+}
+
+public class Cat : Animal
+{
+    public override void GetName ()
+    {
+        Console.WriteLine($"Cat name is {Name}, and age is {Age}");
+    }
+}
+
+public class Operation
+{
+    public static void Call (Dog dog)
+    {
+        Console.WriteLine($"Dog is called, {dog.Age}");
+    }
+
+    public static void Call (Cat cat)
+    {
+        Console.WriteLine($"Cat is called, {cat.Age}");
+    }
+}
