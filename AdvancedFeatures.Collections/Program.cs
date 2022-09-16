7099c5dc9bae0150fd137ea62b7ab75b23e6618b
Avjol Sakaj <avjol.sakaj@cigen.me>
2022-09-16 20:40:43 +0200

    Collection and exceptions

---
+++ AdvancedFeatures.Collections/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace AdvancedFeatures.Collections
+{
+    internal class Program
+    {
+        private static void Main (string[] args)
+        {
+            Console.WriteLine("Dictionary: ");
+
+            var dictionary = new Dictionary<string, int>();
+
+            dictionary.Add("Cat", 2);
+            dictionary.Add("Dog", 1);
+            dictionary.Add("Bird", 0);
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine("{0}, {1}", item.Key, item.Value);
+            }
+
+            Console.WriteLine("Dictionary: ");
+
+            var list = new List<int>();
+            list.Add(2);
+            list.Add(3);
+            list.Add(5);
+            list.Add(7);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Queue: ");
+
+            var queue = new Queue<string>();
+            queue.Enqueue("One");
+            queue.Enqueue("Two");
+            queue.Enqueue("Three");
+            queue.Enqueue("Four");
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+                // queue.Dequeue();
+            }
+
+            Console.WriteLine("stack: ");
+
+            var stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(10);
+            stack.Push(100);
+            stack.Push(1000);
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+                // stack.Pop();
+            }
+        }
+    }
+}
---
+++ AdvancedFeatures.Exceptions/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace AdvancedFeatures.Exceptions;
+
+internal class Program
+{
+    private static void Main (string[] args)
+    {
+        var s = Console.ReadLine();
+
+        try
+        {
+            _ = int.Parse(s);
+            Console.WriteLine("You entered valid number");
+
+            //_ = Sqrt(-1);
+            //Console.WriteLine("This is after sqrt!");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid integer number!");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Number is to big for int");
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.Error.WriteLine("Error: " + ex.Message);
+            throw;
+        }
+    }
+
+    public static double Sqrt (double value)
+    {
+        if (value < 0)
+        {
+            throw new ArgumentOutOfRangeException("Sqrt for negative numbers is undefined");
+        }
+
+        return Math.Sqrt(value);
+    }
+}
